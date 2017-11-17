d5f2bff6f07652559310c1761342617c8ae9309c
ardgz <mario2485@yahoo.com>
2017-11-16 23:08:07 -0600

    Edit keymap and add new local config file and setting

--- keyboards/planck/keymaps/adan/keymap.c
+++ keyboards/planck/keymaps/adan/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* === LOWER === */
 [1] = {
   {KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,   KC_8,   KC_9,    KC_0,    KC_BSPC},
-  {KC_DEL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,   KC_5,   KC_6,    KC_PPLS, KC_PMNS},
+  {_______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4,   KC_5,   KC_6,    KC_PPLS, KC_PMNS},
   {_______,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1,   KC_2,   KC_3,    KC_PAST, KC_PSLS},
   {_______,  _______, _______, _______, _______, _______, _______, KC_0,   KC_0,   KC_DOT,  _______, _______}
 },
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* === RAISE === */
 [2] = {
   {KC_TILD,  KC_EXLM,   KC_AT,     KC_HASH,   KC_DLR,    KC_PERC,   KC_CIRC,   KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
-  {_______,  KC_LCBR,   KC_RCBR,   KC_LPRN,   KC_RPRN,   _______,   _______,   KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  KC_BSLS},
-  {_______,  _______,   _______,   _______,   _______,   _______,   _______,   _______, _______, _______, _______, KC_RSFT},
+  {_______,  KC_LBRC,   KC_RBRC,   KC_LPRN,   KC_RPRN,   _______,   _______,   KC_MINS, KC_EQL,  KC_MINS, KC_EQL,  KC_BSLS},
+  {_______,  KC_LCBR,   KC_RCBR,   _______,   _______,   _______,   _______,   KC_UNDS, KC_PLUS, KC_UNDS, KC_PLUS, KC_RSFT},
   {_______,  _______,   _______,   _______,   _______,   _______,   _______,   _______, _______, _______, _______, _______}
 },
 
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {_______, CAD,     _______, _______, _______, _______, _______, KC_PGUP, KC_UP,   KC_PGDN,  KC_PSCR, _______},
   {_______, M(3),    _______, _______, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_INS,   KC_DEL},
   {_______, _______, _______, _______, _______, _______, KC_END,  _______, _______, _______,  _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______}
 },
 
 
